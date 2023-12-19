0e8103d58c2fe9426ae145e54f619fc56b11e3ae
SarikaR12 <143924688+SarikaR12@users.noreply.github.com>
2023-12-19 21:40:51 +0530

    Add files via upload

---
+++ string3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main()
+{
+	int i;
+	char name[]="SARIKA";
+	printf("string of lowercase =");
+	
+	for(i=0;i<=5;i++)
+	{
+		if(name[i]>=97 && i<=122)
+		{
+			printf("%c",name[i]);
+		}
+		else
+		{
+			printf("%c",name[i]+32);
+		}
+	}
+	
+}
